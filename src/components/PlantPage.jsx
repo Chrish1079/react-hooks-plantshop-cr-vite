84c41c7831c5dae9bcb70a69b15136aaa26b7139
Chrish1079 <itssuper1079@gmail.com>
2025-11-11 21:36:14 -0500

    submit

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((response) => response.json())
+      .then((data) => {
+        const plantsWithStock = data.map((plant) => ({
+          ...plant,
+          soldOut: plant.soldOut ?? false,
+        }));
+        setPlants(plantsWithStock);
+      });
+  }, []);
+
+  function handleAddPlant(newPlant) {
+    setPlants((currentPlants) => [
+      ...currentPlants,
+      { ...newPlant, soldOut: newPlant.soldOut ?? false },
+    ]);
+  }
+
+  function handleToggleSoldOut(plantId) {
+    setPlants((currentPlants) =>
+      currentPlants.map((plant) =>
+        plant.id === plantId ? { ...plant, soldOut: !plant.soldOut } : plant
+      )
+    );
+  }
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+      <PlantList plants={filteredPlants} onToggleSoldOut={handleToggleSoldOut} />
     </main>
   );
 }
